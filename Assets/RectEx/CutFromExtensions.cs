3bfe96c369be03cfe79b89eb67852ab9dfd62f42
SlavniyTeo <slavniyteo@gmail.com>
2017-10-23 12:44:25 +0300

    CutFromLeft + CutFromTop + tests

--- Assets/RectEx/CutFromExtensions.cs
+++ Assets/RectEx/CutFromExtensions.cs
@@ -5,20 +5,56 @@ namespace RectEx {
     public static class CutFromExtensions {
 
         public static Rect[] CutFromRight(this Rect rect, float width, float space = 5){
+            rect = rect.Abs();
+            float min = Math.Min(rect.xMin, rect.xMax - (space + width));
+
             var first = Rect.MinMaxRect(
-                xmin:rect.xMin,
+                xmin:min,
                 xmax:rect.xMax - (space + width),
                 ymin:rect.yMin,
                 ymax:rect.yMax
-            );
+            ).Abs();
             var second = Rect.MinMaxRect(
                 xmin:first.xMax + space,
                 xmax:rect.xMax,
                 ymin:first.yMin,
                 ymax:first.yMax
+            ).Abs();
+            return new Rect[]{first, second};
+        }
+
+        public static Rect[] CutFromLeft(this Rect rect, float width, float space = 5){
+            var first = Rect.MinMaxRect(
+                xmin:rect.xMin,
+                xmax:rect.xMin + width,
+                ymin:rect.yMin,
+                ymax:rect.yMax
+            );
+            float max = Math.Max(rect.xMax, first.xMax + space);
+            var second = Rect.MinMaxRect(
+                xmin:first.xMax + space,
+                xmax:max,
+                ymin:rect.yMin,
+                ymax:rect.yMax
             );
             return new Rect[]{first, second};
         }
 
+        public static Rect[] CutFromTop(this Rect rect, float height, float space = 5){
+            var first = Rect.MinMaxRect(
+                xmin:rect.xMin,
+                xmax:rect.xMax,
+                ymin:rect.yMin,
+                ymax:height
+            );
+            float max = Math.Max(rect.yMax, first.yMax + space);
+            var second = Rect.MinMaxRect(
+                xmin:rect.xMin,
+                xmax:rect.xMax,
+                ymin:first.yMax + space,
+                ymax:max
+            );
+            return new Rect[]{first, second};
+        }
     }
 }
--- Assets/RectEx/Editor/CutFromTest.cs
+++ Assets/RectEx/Editor/CutFromTest.cs
@@ -7,13 +7,13 @@ namespace RectEx {
 	public class CutFromTest {
 
 		[Test]
-		[TestCaseSource(typeof(AbsSource))]
+		[TestCaseSource(typeof(CutFromRightSource))]
 		public void CutFromRight(Rect from, float width, float space, Rect[] expected) {
 			var actual = from.CutFromRight(width, space);
 			Assert.AreEqual(expected, actual);
 		}
 
-		class AbsSource : IEnumerable {
+		class CutFromRightSource : IEnumerable {
 			public IEnumerator GetEnumerator() {
 				yield return new object[] {
 					new Rect(x:0, y:0, width:50, height:10),
@@ -24,6 +24,132 @@ namespace RectEx {
 						new Rect(x:43, y:0, width:7, height:10)
 					}
 				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					10,
+					-1,
+					new Rect[] {
+						new Rect(x:0, y:0, width:41, height:10),
+						new Rect(x:40, y:0, width:10, height:10)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					55,
+					5,
+					new Rect[] {
+						new Rect(x:-10, y:0, width:0, height:10),
+						new Rect(x:-5, y:0, width:55, height:10)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					55,
+					-15,
+					new Rect[] {
+						new Rect(x:0, y:0, width:10, height:10),
+						new Rect(x:-5, y:0, width:55, height:10)
+					}
+				};
+            
+			}
+		}
+
+		[Test]
+		[TestCaseSource(typeof(CutFromLeftSource))]
+		public void CutFromLeft(Rect from, float width, float space, Rect[] expected) {
+			var actual = from.CutFromLeft(width, space);
+			Assert.AreEqual(expected, actual);
+		}
+
+		class CutFromLeftSource : IEnumerable {
+			public IEnumerator GetEnumerator() {
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					7,
+					3,
+					new Rect[] {
+						new Rect(x:0, y:0, width:7, height:10),
+						new Rect(x:10, y:0, width:40, height:10)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					10,
+					-1,
+					new Rect[] {
+						new Rect(x:0, y:0, width:10, height:10),
+						new Rect(x:9, y:0, width:41, height:10)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					55,
+					5,
+					new Rect[] {
+						new Rect(x:0, y:0, width:55, height:10),
+						new Rect(x:60, y:0, width:0, height:10)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:50, height:10),
+					55,
+					-15,
+					new Rect[] {
+						new Rect(x:0, y:0, width:55, height:10),
+						new Rect(x:40, y:0, width:10, height:10)
+					}
+				};
+            
+			}
+		}
+
+		[Test]
+		[TestCaseSource(typeof(CutFromTopSource))]
+		public void CutFromTop(Rect from, float width, float space, Rect[] expected) {
+			var actual = from.CutFromTop(width, space);
+			Assert.AreEqual(expected, actual);
+		}
+
+		class CutFromTopSource : IEnumerable {
+			public IEnumerator GetEnumerator() {
+				yield return new object[] {
+					new Rect(x:0, y:0, width:10, height:50),
+					7,
+					3,
+					new Rect[] {
+						new Rect(x:0, y:0, width:10, height:7),
+						new Rect(x:0, y:10, width:10, height:40)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:10, height:50),
+					10,
+					-1,
+					new Rect[] {
+						new Rect(x:0, y:0, width:10, height:10),
+						new Rect(x:0, y:9, width:10, height:41)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:10, height:50),
+					55,
+					5,
+					new Rect[] {
+						new Rect(x:0, y:0, width:10, height:55),
+						new Rect(x:0, y:60, width:10, height:0)
+					}
+				};
+				yield return new object[] {
+					new Rect(x:0, y:0, width:10, height:50),
+					55,
+					-15,
+					new Rect[] {
+						new Rect(x:0, y:0, width:10, height:55),
+						new Rect(x:0, y:40, width:10, height:10)
+					}
+				};
+            
 			}
 		}
 	}
