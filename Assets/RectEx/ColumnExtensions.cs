49f0fe3a649bef84ebc6564338db6758325b670e
SlavniyTeo <slavniyteo@gmail.com>
2017-10-24 15:26:13 +0300

    Column: default vertical spaces is set to 2

--- Assets/RectEx/ColumnExtensions.cs
+++ Assets/RectEx/ColumnExtensions.cs
@@ -8,17 +8,17 @@ using RectEx.Internal;
 namespace RectEx {
 	public static class ColumnExtensions {
 
-        public static Rect[] Column(this Rect rect, int count, float space = 5){
+        public static Rect[] Column(this Rect rect, int count, float space = 2){
             rect = rect.Invert();
             var result = rect.Row(count, space);
             return result.Select(x => x.Invert()).ToArray();
         }
 
-		public static Rect[] Column(this Rect rect, float[] weights, float space = 5){
+		public static Rect[] Column(this Rect rect, float[] weights, float space = 2){
 			return Column(rect, weights, null, space);
 		}
 
-		public static Rect[] Column(this Rect rect, float[] weights, float[] widthes, float space = 5) {
+		public static Rect[] Column(this Rect rect, float[] weights, float[] widthes, float space = 2) {
             rect = rect.Invert();
             var result = rect.Row(weights, widthes, space);
             return result.Select(x => x.Invert()).ToArray();
